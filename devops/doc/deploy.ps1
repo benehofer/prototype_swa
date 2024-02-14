809c449d96a3261636ddec2d262f32e00e96bbba
Hofer Benedikt <benedikt.hofer@wagner.ch>
2024-02-14 18:37:52 +0100

    nine

--- devops/doc/deploy.ps1
+++ devops/doc/deploy.ps1
@@ -1,4 +1,5 @@
 gci
 $swatoken=$(az staticwebapp secrets list --name 'stapp-hofb-wup-tst-sn-01' -o tsv --query "properties.apiKey")
-echo SWATOKEN=$swatoken >> $GITHUB_ENV
+echo "SWATOKEN=$swatoken" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
+
 
