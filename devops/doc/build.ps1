0f1450680d0ad970b972308968d09fad33ae5509
Hofer Benedikt <benedikt.hofer@wagner.ch>
2024-02-14 18:30:07 +0100

    seven

--- devops/doc/build.ps1
+++ devops/doc/build.ps1
@@ -1,5 +1,5 @@
 Write-Host "Building documentation"
-new-item -Path .\deployment\doc -ItemType Directory
-copy-item -Path .\snippets\doc .\deployment\doc
+new-item -Path .\deployment -ItemType Directory | out-null
+copy-item -Path ".\snippets\doc" ".\deployment\" -Recurse
 
 
